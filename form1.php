8c31e5ae76151522895a9111c760648877f50cf0
pratama69 <nj79372@gmail.com>
2019-08-29 16:48:45 +0700

    ss

--- form1.php
+++ form1.php
@@ -1,29 +1,22 @@
 <?php
-if(isset($_POST['hitung'])){
-            $nama = $_POST['nama'];
-            $alamat = $_POST['alamat'];
-            $namabrng = $_POST['nmabrng'];
-            $harga = $_POST['harga'];
-            $jumlah = $_POST['jumlah'];
-            $deskripsi = $_POST['deskripsi'];
-            $Tanggal = $_POST['tglpmbli'];
-			$qty = $_POST['qty'];
-            $total = $harga*$qty;
-              echo"
-				<table border='1'>
-					<tr>
-						<th>Nama Barang</th>
-						<th>Harga</th>
-						<th>Banyaknya</th>
-						<th>Total Harga</th>
-					</tr>
-					<tr>
-						<td>$nama</td>
-						<td>$harga</td>
-						<td>$qty</td>
-						<td>$total</td>
-					</tr>
-				</table>
-			";
-		}
+if (isset($_POST['input'])) {
+    $a = $_POST['nama'];
+    $b = $_POST['almt'];
+    $c = $_POST['nmbrng'];
+    $e = $_POST['jmlh'];
+    $harga_brg = $_POST['harga_brg'];
+    $sub_total =$jmlh * $harga_brg;
+    }
+
+    if ($jmlh > 10) {
+       $total = $sub_total - (0.2 * $sub_total);
+    } else if ($jmlh < 5) {
+        $total = 0.1 $sub_total;
+
+    }
+    if ($jmlh < 0) {
+        echo "Nama Costumer : $nama";
+    }
+
+
     ?>
